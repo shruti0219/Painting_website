3bfda7d0aa20cc92f2eba8a2b29459c1fa9af1a7
Shruti <shrutik0219@gmail.com>
2020-07-03 22:45:44 +0530

    Secret Key issue

--- hello/settings.py
+++ hello/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from django.contrib.messages import constants as messages
+from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5c@b7k18b6*jhzt@pddmejxwr5&5bogliqozvw7%6%a)r$0)x@'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
